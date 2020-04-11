8ac5eb72125315b4d0e874be78424b104f405d14
Patteera L <mmookptr@hotmail.com>
2020-04-11 21:44:05 +0700

    Add avoidances

--- facts.pl
+++ facts.pl
@@ -2,7 +2,7 @@ coffee(decaffein).
 coffee(caffein).
 
 syrup(chocolate).
-syrup(vanila).
+syrup(vanilla).
 syrup(caramel).
 
 milk(normalMilk).
@@ -19,9 +19,11 @@ drink(mocha).
 drink(americano).
 drink(esspresso).
 drinK(caramel_mac).
+
 intensity(1).
 intensity(2).
 intensity(3).
+
 health_condition(overweight).
 health_condition(diabetes).
 health_condition(pregnant).
@@ -29,4 +31,23 @@ health_condition(heart_disease).
 health_condition(lactose_intoleranse).
 health_condition(caffein_addict).
 
+Avoid_syrup(diabetes, vanilla).
+Avoid_syrup(diabetes, chocolate).
+Avoid_syrup(diabetes, caramel).
+Avoid_syrup(overweight, vanilla).
+Avoid_syrup(overweight, chocolate).
+Avoid_syrup(overweight, caramel).
+Avoid_syrup(heart_disease, vanilla).
+Avoid_syrup(heart_disease, chocolate).
+Avoid_syrup(heart_disease, caramel).
+
+Avoid_milk(lactose_intoleranse, normalMilk).
+Avoid_milk(lactose_intoleranse, lowFatMilk).
+Avoid_milk(lactose_intoleranse, nonFatMilk).
+Avoid_milk(lactose_intoleranse, foamMilk).
+Avoid_milk(overweight, normalMilk).
+
+Avoid_caffein(caffein_addict, caffein).
+Avoid_caffein(pregnant, caffein).
+Avoid_caffein(heart_disease,caffein).
 
