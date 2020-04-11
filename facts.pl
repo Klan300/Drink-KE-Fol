4ff637c0e3b1256503547584a73fff002a627f12
Patteera L <mmookptr@hotmail.com>
2020-04-11 21:02:45 +0700

    add something

--- facts.pl
+++ facts.pl
@@ -1,27 +1,27 @@
-coffee(decaffein)
-coffee(caffein)
-syrup(chocolate)
-syrup(vanila)
-syrup(caramel)
-milk(normal_milk)
-milk(low_fat_milk)
-milk(non_fat_milk)
-milk(non_lactose_ilk)
-milk(foam_milk)
+coffee(decaffein).
+coffee(caffein).
+syrup(chocolate).
+syrup(vanila).
+syrup(caramel).
+milk(normal_milk).
+milk(low_fat_milk).
+milk(non_fat_milk).
+milk(non_lactose_ilk).
+milk(foam_milk).
 drink(latte).
 drink(cappuccino).
 drink(mocha).
 drink(americano).
 drink(esspresso).
 drinK(caramel_mac).
-intensity(1)
-intensity(2)
-intensity(3)
-health_condition(overweight)
-health_condition(diabetes)
-health_condition(pregnant)
-health_condition(heart_disease)
-health_condition(lactose_intoleranse)
+intensity(1).
+intensity(2).
+intensity(3).
+health_condition(overweight).
+health_condition(diabetes).
+health_condition(pregnant).
+health_condition(heart_disease).
+health_condition(lactose_intoleranse).
 
 
 
